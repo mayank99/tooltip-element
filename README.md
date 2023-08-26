fcfb8c8545015f2fb1478857e308719c2478dc67
Mayank <9084735+mayank99@users.noreply.github.com>
2023-08-26 18:54:45 +0000

    make it so

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # tooltip-element
+
+An accessible tooltip built using web components and `@floating-ui/dom`.
+
+See `Tooltip.js` for source code and `index.html` for example usage.
+
+Codesandbox demo: https://codesandbox.io/s/github/mayank99/tooltip-element
+
+## Usage
+
+Import the Tooltip class and call `.register()` on it, optionally passing `tooltipTag`/`triggerTag` into it.
+```js
+import { Tooltip } from './Tooltip.js';
+Tooltip.register();
+```
+
+Now the tooltip can be used by wrapping it around the element that should trigger it on hover/focus. The `text` attribute will be used to populate the tooltip text. By default, tooltip does not add any ARIA attributes, so the trigger element must bring its own label.
+
+```html
+<tool-tip text='Save file' style='display: contents'>
+	<button>
+		<span aria-hidden='true'>💾</span>
+		<span class='visually-hidden'>Save file</span>
+	</button>
+</tool-tip>
+```
+
+Alternatively, the tooltip can be automatically be associated with the trigger using `aria-labelledby`. To opt into this, pass `aria="description"`.
+
+```html
+<tool-tip
+	text='Deletion might take a minute'
+	aria='description'
+	style='display: contents'
+>
+	<button>Delete project</button>
+</tool-tip>
+```
+
+Important: `display: contents` must be set in CSS or as an inline style, in order to keep the custom element from participating in layout.
---
+++ Tooltip.js
@@ -0,0 +1,153 @@
+import { computePosition, flip, shift, offset } from '@floating-ui/dom';
+
+function roundByDPR(value) {
+	const dpr = window.devicePixelRatio || 1;
+	return Math.round(value * dpr) / dpr;
+}
+
+const uniqueId = (() => {
+	let count = 0;
+	return () => `__tt-${++count}`;
+})();
+
+export class Tooltip extends HTMLElement {
+	constructor() {
+		super();
+		this.tooltipName = this.getAttribute('tooltip-tag') || 'tool-tip-text';
+	}
+
+	static register({ triggerTag = 'tool-tip', tooltipTag = 'tool-tip-text' } = {}) {
+		customElements.define(triggerTag, this);
+		this.addStyles(tooltipTag);
+	}
+
+	connectedCallback() {
+		if (!this.firstElementChild) return;
+		this.trigger = this.firstElementChild;
+
+		this.tooltip = document.createElement(this.tooltipName);
+		this.tooltip.id = uniqueId();
+		this.tooltip.textContent = this.getAttribute('text');
+		this.tooltip.setAttribute('aria-hidden', 'true');
+		this.tooltip.hidden = true;
+		this.ownerDocument.body.insertBefore(this.tooltip, null);
+
+		if (this.getAttribute('aria') === 'description') {
+			this.trigger.setAttribute('aria-describedby', this.tooltip.id);
+		}
+
+		this.setupEventHandlers();
+	}
+
+	setupEventHandlers() {
+		function hovered() {
+			this.hovered = true;
+			this.show();
+		}
+		function unhovered() {
+			this.hovered = false;
+			this.hide();
+		}
+		function focused() {
+			this.focused = true;
+			this.show();
+		}
+		function unfocused() {
+			this.focused = false;
+			this.hide();
+		}
+
+		if (matchMedia('(hover: hover)').matches) {
+			this.trigger.addEventListener('pointerenter', hovered.bind(this));
+			this.trigger.addEventListener('pointerleave', unhovered.bind(this));
+		}
+
+		this.tooltip.addEventListener('pointerenter', hovered.bind(this));
+		this.tooltip.addEventListener('pointerleave', unhovered.bind(this));
+		this.trigger.addEventListener('focus', focused.bind(this));
+		this.trigger.addEventListener('blur', unfocused.bind(this));
+
+		this.ownerDocument.addEventListener(
+			'keydown',
+			function ({ key, ctrlKey }) {
+				if (key === 'Escape' || ctrlKey) {
+					this.tooltip.hidden = true;
+				}
+			}.bind(this)
+		);
+	}
+
+	async show() {
+		await new Promise((resolve) => setTimeout(resolve, 100));
+
+		if (!this.hovered && !this.focused) return;
+		this.tooltip.hidden = false;
+
+		const { x, y } = await computePosition(this.trigger, this.tooltip, {
+			placement: 'top',
+			middleware: [offset(4), flip(), shift({ padding: 4 })],
+		});
+		this.tooltip.style.transform = `translate(${roundByDPR(x)}px,${roundByDPR(y)}px)`;
+	}
+
+	async hide() {
+		await new Promise((resolve) => setTimeout(resolve, 100));
+		if (this.focused || this.hovered) return;
+		this.tooltip.hidden = true;
+	}
+
+	static addStyles(tooltipTag) {
+		const cssText = `@layer ${tooltipTag} {
+				${tooltipTag} {
+					box-sizing: border-box;
+					display: block;
+					position: absolute;
+					inset-inline-start: 0;
+					inset-block-start: 0;
+					color: white;
+					background: hsl(0 0% 0% / 0.5);
+					backdrop-filter: blur(5px);
+					border: 1px solid hsl(0 0% 50%);
+					padding-inline: 8px;
+					padding-block: 4px;
+					border-radius: 4px;
+					font-size: max(90%, 1rem);
+					inline-size: max-content;
+					max-inline-size: min(90vi, 30ch);
+					z-index: 9999;
+				}
+				${tooltipTag}[hidden] {
+					display: none !important;
+				}
+				${tooltipTag}::before {
+					content: '';
+					position: absolute;
+					inset: -5px;
+					z-index: -1;
+				}
+			}`;
+		const sheet = new CSSStyleSheet();
+		sheet.replaceSync(cssText);
+		document.adoptedStyleSheets = [...document.adoptedStyleSheets, sheet];
+	}
+
+	static get observedAttributes() {
+		return ['text'];
+	}
+
+	attributeChangedCallback(name, old, value) {
+		if (name === 'text' && this.tooltip) {
+			this.tooltip.textContent = this.getAttribute('text');
+		}
+		if (name === 'aria' && value === 'description') {
+			this.trigger?.setAttribute('aria-describedby', this.tooltip.id);
+		}
+		if (name === 'aria' && old === 'description' && value !== 'description') {
+			this.trigger?.removeAttribute('aria-describedby');
+		}
+	}
+
+	disconnectedCallback() {
+		this.tooltip.remove();
+	}
+}
